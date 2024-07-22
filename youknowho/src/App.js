d5fd90947f9ccbf22479f7218aa0eec95408c4f7
anyeeah <anyaehyun3438@gmail.com>
2024-07-22 23:08:24 +0900

    [Docs] 파일 구조 짜고 라우팅

--- youknowho/src/App.js
+++ youknowho/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { Layout } from './component/layout/Layout';
+import { Login } from './pages/login/Login';
+import { Mypage } from './pages/mypage/Mypage';
+import { SearchHome } from './pages/searchHome/SearchHome';
+import { Signup } from './pages/signup/Signup';
+import { Localnews } from './pages/localnews/Localnews';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+    return (
+            <Routes>
+                <Route path="/" element={<Layout />} >
+                  <Route index element={<Home />} />
+                  <Route path="login" element={<Login />} />
+                  <Route path="mypage" element={<Mypage />} />
+                  <Route path="searchhome" element={<SearchHome />} />
+                  <Route path="signup" element={<Signup />} />
+                  <Route path="localnews" element={<Localnews />} />
+                </Route>
+            </Routes>
+    );
 }
 
 export default App;
---
+++ youknowho/src/pages/localnews/Localnews.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Localnews = () => {
+  return (
+    <div>Localnews</div>
+  )
+}
