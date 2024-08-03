7e64b11dc0c32542436dc217eb076112ce3a0c1a
anyeeah <anyaehyun3438@gmail.com>
2024-08-04 01:09:38 +0900

    [Test] 카카오 로그인

--- src/component/RouterChangeListener.jsx
+++ src/component/RouterChangeListener.jsx
@@ -7,10 +7,12 @@ import { curPageRecoil } from '../recoil/atom';
 const RouteChangeListener = () => {
   const router = useLocation();
   const [curPage, setCurPage] = useRecoilState(curPageRecoil);
+  //recoil로 로그인 상태 저장
 
   //페이지 변경 시
   useEffect(() => {
     setCurPage(router.pathname);
+    //토큰 만료 시 로그아웃
   }, [router]);
 
   return null; 
--- src/pages/login/KakaoLogin.jsx
+++ src/pages/login/KakaoLogin.jsx
@@ -1,25 +1,40 @@
 import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+const baseURL = 'https://wellcheers.p-e.kr'; // 백엔드 URL
 
 const KakaoLogin = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
     const code = new URL(window.location.href).searchParams.get('code');
+    console.log('인가 코드:', code); // 인가 코드 확인
 
     if (code) {
       const postCode = async () => {
+        const body = JSON.stringify({ code }); // body를 JSON 문자열로 변환
         try {
-          const response = await axios.post('<http://localhost:5000/api/auth/kakao>', { code });
+          const response = await axios.post(`${baseURL}/account/kakao/callback/`, body, {
+            headers: {
+              'Content-Type': 'application/json', // JSON 형식으로 데이터 전송
+            },
+          });
+
+          // 응답에서 accessToken과 refreshToken 추출
           const { accessToken, refreshToken } = response.data;
 
+          // localStorage에 토큰 저장
           localStorage.setItem('access', accessToken);
           localStorage.setItem('refresh', refreshToken);
 
+          // /test 페이지로 이동
           navigate('/');
         } catch (error) {
-          console.error('Error during Kakao login', error);
+          console.error('인가 코드 전송 중 에러 발생', error);
+          if (error.response) {
+            console.error('Error response:', error.response);
+          }
         }
       };
       postCode();
